d1be6d034bac4f0edf49e491e0f78df0c0583e16
JoshSevy <joshuasevy@outlook.com>
2021-06-26 11:12:32 -0600

    Add bp for form and app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { User } from './auth-form/auth-form.interface';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'ng-pro';
+
+  createUser(user: User) {
+    console.log('Create account', user);
+  }
+
+  loginUser(user: User) {
+    console.log('Login', user);
+  }
+
 }
