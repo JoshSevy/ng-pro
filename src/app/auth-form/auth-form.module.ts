d94261684f7ca2770ab09dfd7cb8fd030d100fe9
JoshSevy <joshuasevy@outlook.com>
2021-06-26 17:42:12 -0600

    Add auth remember

--- src/app/auth-form/auth-form.module.ts
+++ src/app/auth-form/auth-form.module.ts
@@ -3,10 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { AuthFormComponent } from './auth-form.component';
+import { AuthRememberComponent } from './auth-remember/auth-remember.component';
 
 @NgModule({
   declarations: [
-    AuthFormComponent
+    AuthFormComponent,
+    AuthRememberComponent
   ],
   imports: [
     CommonModule,
