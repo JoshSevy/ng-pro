a50a0c4bac4d42195e8c02762c200bd66bfeff97
JoshSevy <joshuasevy@outlook.com>
2021-07-01 20:20:33 -0600

    Add component

---
+++ src/app/auth-form/auth-message/auth-message.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-message',
+  templateUrl: './auth-message.component.html',
+})
+export class AuthMessageComponent {
+  days: number = 7;
+}
