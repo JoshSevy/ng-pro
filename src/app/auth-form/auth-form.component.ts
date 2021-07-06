9c21f65f72dc981263297e978ec5e89ccb5aa15c
JoshSevy <joshuasevy@outlook.com>
2021-07-05 20:41:09 -0600

    Fix error with change detection

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, ContentChildren, AfterContentInit, QueryList, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, Output, EventEmitter, ContentChildren, AfterContentInit, QueryList, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 
 import { AuthRememberComponent } from './auth-remember/auth-remember.component';
 import { AuthMessageComponent } from './auth-message/auth-message.component';
@@ -13,20 +13,23 @@ export class AuthFormComponent implements AfterContentInit, AfterViewInit {
 
   showMessage: boolean;
 
-  @ViewChild(AuthMessageComponent) message: AuthMessageComponent;
+  @ViewChild(AuthMessageComponent) message: QueryList<AuthMessageComponent>;
 
   @ContentChildren(AuthRememberComponent) remember: QueryList<AuthRememberComponent>;
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
 
+  constructor(cd: ChangeDetectorRef) {}
+
   ngAfterViewInit() {
-    // this.message.days = 30;
+    if (this.message) {
+      this.message.forEach((message) => {
+        message.days = 30;
+      });
+      this.cd.detectChanges();
   }
+}
 
   ngAfterContentInit() {
-    if (this.message) {
-      this.message.days = 30;
-    }
-
     if (this.remember) {
       this.remember.forEach((query) => {
         query.checked.subscribe((checked: boolean) => this.showMessage = checked);
