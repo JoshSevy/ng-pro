65a4261a3335ca7f4e223dfc853b5084fd88a13d
JoshSevy <joshuasevy@outlook.com>
2021-07-24 20:34:51 -0600

    Inject pipe to transform in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FileSizePipe } from './filesize.pipe';
+
 interface File {
   name: string,
   size: number,
@@ -9,10 +11,14 @@ interface File {
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  providers: [FileSizePipe]
 })
 export class AppComponent implements OnInit {
   files: Array<File> = [];
+  mapped = [];
+
+  constructor(private fileSizePipe: FileSizePipe) {}
 
   ngOnInit() {
     this.files = [
@@ -20,5 +26,13 @@ export class AppComponent implements OnInit {
       { name: 'banner.jpg', size: 18029, type: 'image/jpg' },
       { name: 'background.png', size: 1784562, type: 'image/png' }
     ];
+
+    this.mapped = this.files.map(file => {
+      return {
+        name: file.name,
+        type: file.type,
+        size: this.fileSizePipe.transform(file.size, 'mb')
+      };
+    });
   }
 }
