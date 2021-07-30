a595051608d3ec04904d857ee6892bd236e02c0d
JoshSevy <joshuasevy@outlook.com>
2021-07-29 20:47:54 -0600

    Add base for stock-branch

---
+++ src/app/stock-inventory/components/stock-branch/stock-branch.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "stock-branch",
+    templateUrl: "./stock-branch.component.html",
+    styleUrls: ["./stock-branch.component.css"]
+})
+export class StockBranchComponent {
+}
