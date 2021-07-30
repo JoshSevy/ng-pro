765f2b825d5503efdd368a3bd771059c3485d882
JoshSevy <joshuasevy@outlook.com>
2021-07-29 20:22:56 -0600

    Update form component boiler plate

--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,11 +1,22 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, FormArray } from "@angular/forms";
 
-Component({
+@Component({
     selector: "stock-inventory",
     templateUrl: "./stock-inventory.component.html",
-    styleUrls: ["./stock-inventory.component.css"]
+    styleUrls: ["./stock-inventory.component.scss"]
 })
 export class StockInventoryComponent {
+  form = new FormGroup({
+    store: new FormGroup({
+      branch: new FormControl(""),
+      code: new FormControl("")
+    })
+  })
+
+  onSubmit() {
+    console.log('Submit:', this.form.value);
+  }
 
     constructor() { }
 }
--- src/app/stock-inventory/stock-inventory.module.ts
+++ src/app/stock-inventory/stock-inventory.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { StockInventoryComponent } from './containers/stock-inventory/stock-inventory.component';
 
-import { StockInventoryComponent } from '.containers/stock-inventory/stock-inventory.component';
+
 
 @NgModule({
   declarations: [
