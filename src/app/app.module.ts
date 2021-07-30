cfe8662078e2e3d3d7f50fb80263bf980f54cd24
JoshSevy <joshuasevy@outlook.com>
2021-07-29 19:50:14 -0600

    Setup reactive forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,20 @@ import { CreditCardDirective } from './credit-card/credit-card.directive';
 import { FileSizePipe } from './filesize.pipe';
 import { TooltipDirective } from './tooltip/tooltip.directive';
 
+import { StockInventoryModule } from './stock-inventory/stock-inventory.module';
+
 @NgModule({
   declarations: [
     AppComponent,
     CreditCardDirective,
     TooltipDirective,
     FileSizePipe
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AuthFormModule,
+    StockInventoryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+Component({
+    selector: "stock-inventory",
+    templateUrl: "./stock-inventory.component.html",
+    styleUrls: ["./stock-inventory.component.css"]
+})
+export class StockInventoryComponent {
+
+    constructor() { }
+}
