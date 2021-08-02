dc37ccf1b272649ad340c42146e3716e05074b01
JoshSevy <joshuasevy@outlook.com>
2021-08-01 23:04:07 -0600

    Add selector html

--- src/app/stock-inventory/components/stock-products/stock-products.component.ts
+++ src/app/stock-inventory/components/stock-products/stock-products.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Product } from '../../models/product.interface';
 
 @Component({
   selector: 'stock-products',
@@ -9,4 +10,6 @@ import { FormGroup } from '@angular/forms';
 export class StockProductsComponent {
   @Input()
   parent: FormGroup;
+  @Input()
+  products: Product[];
 }
--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { FormControl, FormGroup, FormArray } from "@angular/forms";
+import { Product } from "../../models/product.interface";
 
 @Component({
     selector: "stock-inventory",
@@ -7,6 +8,14 @@ import { FormControl, FormGroup, FormArray } from "@angular/forms";
     styleUrls: ["./stock-inventory.component.scss"]
 })
 export class StockInventoryComponent {
+  products: Product[] = [
+    { "id": 1, "price": 2800, "name": "MacBook Pro" },
+    { "id": 1, "price": 50, "name": "USB-C Adaptor" },
+    { "id": 1, "price": 400, "name": "iPod" },
+    { "id": 1, "price": 900, "name": "iPhone" },
+    { "id": 1, "price": 600, "name": "Apple Watch" },
+  ];
+
   form = new FormGroup({
     store: new FormGroup({
       branch: new FormControl(""),
